af724657f1a9f58d60af8cb34cc8fa7d3ef3e091
IkuzoTommy <101907933+IkuzoTommy@users.noreply.github.com>
2022-05-26 12:09:29 -0400

    sticky note update

--- Sticky Notes/main.js
+++ Sticky Notes/main.js
@@ -2,23 +2,29 @@ const notesContainer = document.getElementById("app");
 //reference to the main container, that will be used later
 const addNoteButton = notesContainer.querySelector(".add-note");
 //this is just referenceing the add notes button
-getNotes().forEach((note) => {
+
+getNotes().forEach((note) => { //this will display the notes to the user
   const noteElement = createNoteElement(note.id, note.content);
-  notesContainer.insertBefore(noteElement, addNoteButton);
+  notesContainer.insertBefore(noteElement, addNoteButton); //just inserts the note before the add note button
 });
 
 addNoteButton.addEventListener("click", () => addNote());
 
 function getNotes() {
   return JSON.parse(localStorage.getItem("stickynotes-notes") || "[]");
+  //attempt to get all of the notes stored in local storage
+  //for first time users it will just return an empty array
 }
 
 function saveNotes(notes) {
   localStorage.setItem("stickynotes-notes", JSON.stringify(notes));
+  //takes in javascript notes and then makes them a string that will be stored in your local
+  //storage, basically updates the array and then saves it as the same array
 }
 
 function createNoteElement(id, content) {
   const element = document.createElement("textarea");
+  //"element" is a  javascript version of an html element
 
   element.classList.add("note");
   element.value = content;
