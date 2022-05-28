23e3515d62967af6e5c1e9d91979272d5ae8d877
IkuzoTommy <101907933+IkuzoTommy@users.noreply.github.com>
2022-05-27 22:05:58 -0400

    sticky notes update

---
+++ Stickipalooza/main.js
@@ -0,0 +1,15 @@
+const notes = document.getElementById("container");
+const newNoteBttn = document.querySelector("addNoteBttn");
+
+function getNotes(){
+
+}
+
+function saveNote(){
+
+}
+function updateNote (){
+    
+}
+
+
